5b28f261a9a585e23365bdc335205d7cd4c96588
samantha <langridge.se@gmail.com>
2022-03-19 16:01:28 -0600

    created array and loop

--- js/app.js
+++ js/app.js
@@ -1,1 +1,19 @@
-var tweets[``]
+//started with making my array and my loop
+
+var tweets = [
+    `Sick of having to go to 2 different huts to buy pizza & sunglasses - @Leemanish`,
+    `Building inspector: What's this? Darth Vader: the death -- uh the health star - @bobvulfov`,
+    `How dare you call me mentally unstable, on this, the day of my cat's quinceanera - @phranqueigh`,
+    `When I find myself in times of trouble, brother Mario comes to me speaking words of wisdom.. "it's a me" - @ellle_em`,
+    `[Joins a conga line] me: I can leave any time I like  [Someone joins behind] me: Oh no - @HansGruberton`,
+    `"Anybody here named Jeff?"  Jeff: "Yes"   Geoff: "Yeos" - @mtobey`,
+    `"How much for the horse tornado?"    Sir, that's a carousel.   "I must have it." - @neonsinatra`,
+    `"Um." - 1st horse that got ridden - @rorynotroy`,
+    `Our scariest president was probably Rushmore, because he had four heads - @meganamram`,
+    `"Don't worry, I'll hold your stuff. You just worry about making friends." - Cargo shorts   - @DearAnyone`
+]
+// loop is starting at 0, when the counter hits 9 the printing loop ends.
+
+for (i=0; i < 9; i++){
+    console.log(tweets[i]);
+}
